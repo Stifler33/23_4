caa832884f7120f47f27f06f970d2615df01e1bf
stifler33 <bagi33.ru@gmail.com>
2021-10-16 11:02:36 +0300

    task3 finish 1.0

--- task3.cpp
+++ task3.cpp
@@ -1,4 +1,29 @@
-//
-// Created by bagi3 on 11.10.2021.
-//
-
+#include <iostream>
+#define WINTER 0
+#define SPRING 1
+#define SUMMER 0
+#define AUTUMN 0
+#if WINTER
+int main() {
+    std::cout << "Winter\n";
+    return 0;
+}
+#endif
+#if SPRING
+int main() {
+    std::cout << "Spring\n";
+    return 0;
+}
+#endif
+#if SUMMER
+int main() {
+    std::cout << "Summer\n";
+    return 0;
+}
+#endif
+#if AUTUMN
+int main() {
+    std::cout << "Autumn\n";
+    return 0;
+}
+#endif
