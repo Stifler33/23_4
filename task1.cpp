b5ac4c42726af77a649f4c290f11101184c9da5d
stifler33 <bagi33.ru@gmail.com>
2021-10-16 11:22:42 +0300

    added task 1

--- task1.cpp
+++ task1.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 #include <map>
-#define MON_DAY "monday"
-#define TUES_DAY "tuesday"
-#define WEDNES_DAY "wednesday"
-#define THURS_DAY "thursday"
-#define FRI_DAY "friday"
-#define SATUR_DAY "saturday"
-#define SUN_DAY "sunday"
-
-#define WEEK(number)
-
+#define WEEK(number) std::map<int, std::string> week;\
+week.insert(std::pair(1, "monday"));\
+week.insert(std::pair(2, "tuesday"));\
+week.insert(std::pair(3, "wednesday"));\
+week.insert(std::pair(4, "thursday"));\
+week.insert(std::pair(5, "friday"));\
+week.insert(std::pair(6, "saturday"));\
+week.insert(std::pair(7, "sunday"));\
+std::cout << week[number];
 int main() {
     int numberDay;
     std::cout << "Enter number day\n";
     std::cin >> numberDay;
-    std::string str = WEEK(1);
+    WEEK(numberDay);
     return 0;
 }
